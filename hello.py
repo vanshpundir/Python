54e5fd236357d8d1669e5bbeeb37ad6d782bb17d
vanshpundir <40695064+vanshpundir@users.noreply.github.com>
2021-10-03 21:05:16 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-print('works')
-a = 4
-print(a)
+# Write your code here
+x1 = int(input())
+y1 = int(input())
+
+x2 = int(input())
+y2 = int(input())
+
+difference1 = (x2-x1) * (y2-y1)
+print(difference1)
